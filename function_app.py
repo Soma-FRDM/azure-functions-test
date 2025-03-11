59e1fa87cc58322b2a66bc2218ad1d80c3562a6e
TheCodingGrew <s.narayanan@ilac.com>
2025-03-11 17:19:12 -0400

    reinitiating push

--- function_app.py
+++ function_app.py
@@ -6,10 +6,5 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="http_trigger_hello")
 def http_trigger_hello(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse(f"Hello,  my world. This is an attempt at reaching you.")
 
-    return func.HttpResponse(f"Hello,  my world. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
