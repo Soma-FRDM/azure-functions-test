fca906d7f38f5d7a1443d9ce8ca107649c0d5ebc
TheCodingGrew <s.narayanan@ilac.com>
2025-03-11 18:32:17 -0400

    pushing trigger change

--- function_app.py
+++ function_app.py
@@ -7,5 +7,5 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="http_trigger_hello")
 def http_trigger_hello(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(f"Hello,  my world. This is an attempt at reaching you.")
+    return func.HttpResponse(f"Hello,  my world. This is an attempt at reaching you. Where are you ?")
 
